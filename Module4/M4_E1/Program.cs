54cbe0d97406def1b4cc2f390718c198c3c01317
Eric Wenaas <eric.wenaas@bdeb.qc.ca>
2022-09-29 14:15:38 -0400

    A2022: Module 4
    Les solutions pour les exercices 1 et 2 sont terminées.

--- Module4/M4_E1/Program.cs
+++ Module4/M4_E1/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+
+Console.WriteLine("Dans cet exercice, il fallait créer le fichier XML dans un dossier Fichiers-3GP.");
+Console.WriteLine("Bonnne nouvelle !! Ce programme place justement le fichier au bon endroit.");
+
+if (ConfirmerOperation("Voulez-vous placer le fichier au bon endroit"))
+{
+    CopierFichier("contacts.xml");
+}
+else
+{
+    Console.WriteLine("Aucun changement n'est effectué");
+}
+
+bool ConfirmerOperation(string description)
+{
+    Console.Write($"{description} (Oui pour confirmer)? ");
+    string? reponse = Console.ReadLine();
+    return reponse.Equals("Oui");
+}
+
+void CopierFichier(string nomFichier)
+{
+    string pathMesDocuments = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+    string pathDossier = $"{pathMesDocuments}\\Fichiers-3GP";
+    string pathFichier = $"{pathDossier}\\{nomFichier}";
+
+    // Création du dossier s'il n'existe pas
+    if (! Directory.Exists(pathDossier))
+    {
+        Directory.CreateDirectory(pathDossier);
+    }
+
+    bool doitCopier = true;
+
+    // On regarde si le fichier existe
+    if (File.Exists(pathFichier))
+    {
+        doitCopier = ConfirmerOperation("Le fichier existe. Voulez-vous l'écraser");
+    }
+
+    // On regarde s'il faut copier
+    if (doitCopier)
+    {
+        Console.WriteLine($"Écriture du fichier sous {pathFichier}");
+        File.Copy(nomFichier, pathFichier, true);
+    }
+    else
+    {
+        Console.WriteLine("Aucun changement n'est effectué");
+    }
+}
---
+++ Module4/M4_E2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Runtime.ExceptionServices;
+using System.Xml;
+
+string pathFichier = $"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\\Fichiers-3GP\\contacts.xml";
+
+// On vérifie si le fichier contacts.xml existe 
+if (! File.Exists(pathFichier))
+{
+    Console.Error.WriteLine($"Le fichier {pathFichier} n'existe pas");
+    System.Environment.Exit(1);
+}
+
+
+// Chargement des fichiers
+
+XmlDocument document = new XmlDocument();
+document.Load(pathFichier);
+
+
+// Récupération de la racine
+XmlElement root = document.DocumentElement;
+
+// Affichage du nombre d'éléments avec la balise contact
+XmlNodeList elements = root.GetElementsByTagName("contact");
+Console.WriteLine($"Il y a {elements.Count} contacts dans le fichier.");
+
+// Affichage du nombre de contacts qui ont Simpson comme nom de famille
+int nombreSimpsons = 0;
+foreach (XmlNode node in elements)
+{
+    XmlElement? element = node as XmlElement;
+    if (element != null)
+    {
+        if (element.GetAttribute("nom").Equals("Simpson"))
+        {
+            nombreSimpsons++;
+        }
+    }
+}
+
+Console.WriteLine($"Il y a {nombreSimpsons} Simpson dans les contacts.");
+
+// On affiche l'adresse de Ned Flanders
+XmlElement? elementNed = null;
+foreach (XmlNode node in elements)
+{
+    XmlElement? element = node as XmlElement;
+    if (element != null)
+    {
+        if (element.GetAttribute("nom").Equals("Flanders"))
+        {
+            elementNed = element;
+            break;
+        }
+    }
+}
+
+if (elementNed != null)
+{
+    XmlElement adresse = elementNed["adresse"];
+    string numero = adresse.GetAttribute("numero");
+    string rue = adresse.GetAttribute("rue");
+    Console.WriteLine($"L'adresse de Ned Flanders est {numero} {rue}.");
+}
+
+// Affichage de la description de monsieur Burns
+XmlElement? elementBurns = null;
+foreach (XmlNode node in elements)
+{
+    XmlElement? element = node as XmlElement;
+    if (element != null)
+    {
+        if (element.GetAttribute("nom").Equals("Burns"))
+        {
+            elementBurns = element;
+            break;
+        }
+    }
+}
+
+if (elementBurns != null)
+{
+    XmlElement description = elementBurns["description"];
+    Console.WriteLine($"La description de monsier Burns est:\n{description.InnerText.Trim()}");
+}
+
