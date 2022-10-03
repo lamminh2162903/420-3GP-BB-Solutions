9640801695933bf388208fb0de4b93a139cd679f
Eric Wenaas <eric.wenaas@bdeb.qc.ca>
2022-10-03 15:13:57 -0400

    A2022: Module 4
    Solution pour le module 4 sont terminées.

---
+++ Module4/ClassesAffaires/IXMLSerializable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Utilitaires
+{
+    public interface IXMLSerializable
+    {
+        public XmlElement ToXML(XmlDocument doc);
+        public void FromXML(XmlElement elem);
+    }
+}
+
+
---
+++ Module4/M4_E4/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Xml;
+using System.Xml.Linq;
+using Contacts;
+using Utilitaires;
+
+string pathFichier = $"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\\Fichiers-3GP\\contacts.xml";
+
+// On vérifie si le fichier contacts.xml existe 
+if (!File.Exists(pathFichier))
+{
+    Console.Error.WriteLine($"Le fichier {pathFichier} n'existe pas");
+    System.Environment.Exit(1);
+}
+
+List<Contact> contacts = LireContacts(pathFichier);
+AfficherContacts(contacts);
+SauvegarderContacts(contacts, pathFichier);
+
+
+// Lit le fichier XML et met les contacts dans la liste
+List<Contact> LireContacts(string pathFichier) 
+{
+    // Chargement du fichier
+    XmlDocument doc = new XmlDocument();
+    doc.Load(pathFichier);
+
+    XmlElement root = doc.DocumentElement;
+
+    List<Contact> lesContacts = new List<Contact>();
+    XmlNodeList noeudsContacts = root.GetElementsByTagName("contact");
+    foreach(XmlNode noeud in noeudsContacts)
+    {
+        XmlElement elementContact = noeud as XmlElement;
+        lesContacts.Add(new Contact(elementContact));
+    }
+    return lesContacts;
+}
+
+// Affiche les contacts dans la liste
+void AfficherContacts(List<Contact> lesContacts)
+{
+    foreach(Contact contact in lesContacts)
+    {
+        Console.WriteLine(contact.ToString());
+        Console.WriteLine();
+    }
+}
+
+// Sauvegarde la liste de contacts de la liste dans un fichier xml
+void SauvegarderContacts(List<Contact> lesContacts, string pathFichier)
+{
+    XmlDocument document = new XmlDocument();
+    XmlElement racine = document.CreateElement("contacts");
+    document.AppendChild(racine);
+    
+    foreach(Contact c in lesContacts)
+    {
+        XmlElement element = c.ToXML(document);
+        racine.AppendChild(element);
+    }
+    document.Save(pathFichier);
+
+}
