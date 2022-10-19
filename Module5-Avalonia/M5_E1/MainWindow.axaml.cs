4b3887ecee1363a18399f6159c9e46d4870252e4
ewenaas <ewenaas@bdeb.qc.ca>
2022-10-19 13:45:13 -0400

    Exercice 1 de Avalonia fonctionnel

---
+++ Module5-Avalonia/M5_E1/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace M5_E1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
