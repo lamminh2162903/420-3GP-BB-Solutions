f42316d737d9cb3ceb8a648afaf32c2e9b14db50
ewenaas <ewenaas@bdeb.qc.ca>
2022-10-25 23:52:34 -0400

    Exercice #3 est complété. Le positionnement des boutons laisse toutefois à désirer

---
+++ Module6-Avalonia/M6_E3/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace M6_E3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
