1a9ac264c28d76582517905bdc89d4153e6f8890
ewenaas <ewenaas@bdeb.qc.ca>
2022-10-25 23:16:22 -0400

    Exercice 2 complété avec Avalonia

---
+++ Module6-Avalonia/M6_E2/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace M6_E2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
