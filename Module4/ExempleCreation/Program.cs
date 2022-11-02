7ff2b7d7956f8d3db82a63d3359868976dea806f
Eric Wenaas <eric.wenaas@bdeb.qc.ca>
2022-11-02 17:45:29 -0400

    Correction de l'exercice 6

---
+++ Module4/ExempleCreation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml;
+
+XmlDocument doc = new XmlDocument();
+
+char DIR_SEPARATOR = Path.DirectorySeparatorChar;
+
+string pathFichier = $"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}{DIR_SEPARATOR}" +
+                     $"Fichiers-3GP{DIR_SEPARATOR}exemple.xml";
+
+
+
+XmlElement racine = doc.CreateElement("racine");
+doc.AppendChild(racine);
+
+XmlElement fils = doc.CreateElement("fils");
+fils.SetAttribute("attribut", "valeur");
+fils.InnerText = "Un texte dans le fils";
+
+racine.AppendChild(fils);
+
+doc.Save(pathFichier);
